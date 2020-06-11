cdda542a3b74c8888642146d64fed2d82ff1e7d9
Dylan <dylan.cobean@gmail.com>
2020-06-11 03:47:20 -0700

    + | SML | Tweak/CSS/Mobile -- Button sizing was tiny for Mapbox Controls, fixed that for the most part

---
+++ js/apikeys.js
@@ -0,0 +1,7 @@
+//My Private APIs to list as variables
+
+var API = {
+	"mapbox":"pk.eyJ1IjoiZHlsYW5jIiwiYSI6Im53UGgtaVEifQ.RJiPqXwEtCLTLl-Vmd1GWQ",
+	"ors":"5b3ce3597851110001cf6248a3b9853ae85f4890a9323dccdefc3e65",
+	"graphhopper":"7bca7e42-39c5-4e81-bb5d-bbc3c05ae5ed",
+}
